c7e1e8e9953cca5702f9f0e88c80e8545dc75d1b
Vale Acosta <1hacostas1@gmail.com>
2023-08-12 20:13:05 -0300

    solucion

--- eventos.js
+++ eventos.js
@@ -1,4 +1,10 @@
-let div= document.getElementById('alert');
-div.addEventListener("click",()=>{
-    alert('Hola! Soy el div');
-})
+    let div= document.getElementById('alert');
+    div.addEventListener("click",()=>{
+        alert('Hola! Soy el div');
+    })
+
+    let button = document.getElementById('button');
+    button.addEventListener("click", ()=>{
+        alert('Hola!');
+        event.stopPropagation();
+    })
